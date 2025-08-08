9e7f0e2261551f31b22e2bf2f035ecc083a8ba93
KassFequet <kassaundra.fequet@keyin.com>
2025-08-08 10:29:35 -0230

    moved images

--- momandpop/src/App.jsx
+++ momandpop/src/App.jsx
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import ProductDetails from './pages/ProductDetails';
 import Home from './pages/Home';
 import './App.css';
-import Home from './pages/Home';
 
 // Missing Catalogue and Cart components
 function Catalogue() {
