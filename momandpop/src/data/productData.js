e31b90332a9e778f26d961ae5b00bd5ee57f7e61
Long-Bottom-Leaf <129445517+Long-Bottom-Leaf@users.noreply.github.com>
2025-08-11 21:21:38 -0230

    Create productData.js

---
+++ momandpop/src/data/productData.js
@@ -0,0 +1,31 @@
+export const sections = [
+  {
+    title: "Timber Options",
+    backgroundImage: "https://placehold.co/600x400",
+    menuItems: ["2x4 Inch Planks", "2x6 Inch Planks", "2x8 Inch Planks", "2x10 Inch Planks"]
+  },
+
+  {
+    title: "Wood Working Tools",
+    backgroundImage: "https://placehold.co/600x400",
+    menuItems: ["Chisel", "Hammer"]
+  },
+
+  {
+    title: "Paint and Stain",
+    backgroundImage: "https://placehold.co/600x400",
+    menuItems: ["Exterior Paint", "Interior Stain"]
+  },
+
+  {
+    title: "Garden Tools",
+    backgroundImage: "https://placehold.co/600x400",
+    menuItems: ["Shovel", "Hoe", "Rake"]
+  },
+
+  {
+    title: "Seeds",
+    backgroundImage: "https://placehold.co/600x400",
+    menuItems: ["Tomato", "Carrot", "Lettuce"]
+  }
+];
