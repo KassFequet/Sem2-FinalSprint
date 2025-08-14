454bf39875d33983cf28dd0b508baad51e2ad840
Long-Bottom-Leaf <129445517+Long-Bottom-Leaf@users.noreply.github.com>
2025-08-14 14:39:20 -0230

    Update App.jsx

--- momandpop/src/App.jsx
+++ momandpop/src/App.jsx
@@ -4,12 +4,9 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
+import Catalogue from "./pages/Catalogue";
 
 // Missing Catalogue and Cart components
-function Catalogue() {
-  return <div>Catalogue Page</div>;
-}
-
 function Cart() {
   return <div>Cart Page</div>;
 }
@@ -22,7 +19,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/Catalogue" element={<Catalogue />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/products/:productId" element={<ProductDetails />} />
       </Routes>
